e759eb5651a44f22bf3761b6b1faaba3a93b9781
Anxo Rodriguez <anxolin@gmail.com>
2021-05-21 14:05:28 +0200

    Add react rules

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,31 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'src/index.html'
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  module: {
+     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ],    
   },
   resolve: {
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
